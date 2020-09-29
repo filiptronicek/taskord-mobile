3f51dc782376b8118f460a8c5e95b4ddf66d80e4
Filip Troníček <filip.tronicek@seznam.cz>
2020-09-29 18:05:23 +0200

    Go to Sign In when token not present

--- home.component.js
+++ home.component.js
@@ -1,19 +1,34 @@
-import React from 'react';
-import { SafeAreaView } from 'react-native';
-import { Button, Divider, Layout, TopNavigation } from '@ui-kitten/components';
+import React from "react";
+import { SafeAreaView } from "react-native";
+import { Button, Divider, Layout, TopNavigation } from "@ui-kitten/components";
+import AsyncStorage from '@react-native-community/async-storage';
 
 export const HomeScreen = ({ navigation }) => {
-
-  const navigateDetails = () => {
-    navigation.navigate('SignIn');
+  const signIn = () => {
+    navigation.navigate("SignIn");
   };
 
+  const getData = async () => {
+    try {
+      const value = await AsyncStorage.getItem("usr_token");
+      if (value !== null) {
+        // value previously stored
+      } else {
+        signIn();
+      }
+    } catch (e) {
+      // error reading value
+    }
+  };
+  getData();
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <TopNavigation title='TASKORD' alignment='center'/>
-      <Divider/>
-      <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Button onPress={navigateDetails}>Sign In</Button>
+      <TopNavigation title="TASKORD" alignment="center" />
+      <Divider />
+      <Layout
+        style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+      >
+        <Button onPress={signIn}>Sign In</Button>
       </Layout>
     </SafeAreaView>
   );
