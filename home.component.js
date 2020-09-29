081c3607e39cdbd24b41a8b18568aa8334b78590
Filip Troníček <filip.tronicek@seznam.cz>
2020-09-29 17:37:38 +0200

    Sign in text

--- home.component.js
+++ home.component.js
@@ -13,7 +13,7 @@ export const HomeScreen = ({ navigation }) => {
       <TopNavigation title='TASKORD' alignment='center'/>
       <Divider/>
       <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Button onPress={navigateDetails}>OPEN DETAILS</Button>
+        <Button onPress={navigateDetails}>Sign In</Button>
       </Layout>
     </SafeAreaView>
   );
