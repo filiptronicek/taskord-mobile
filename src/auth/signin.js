f0b00b7979bc0f542f1f59a6881355cf5289cc09
Filip Troníček <filip.tronicek@seznam.cz>
2020-10-02 15:33:51 +0200

    Create signin.js

---
+++ src/auth/signin.js
@@ -0,0 +1,28 @@
+import { fetch } from 'node-fetch';
+
+const signIn = async(email, password) => {
+    const url = `https://taskord.com/graphql`;
+    const query = `
+        mutation {
+            login(email: "${email}", password: "${password}") {
+            token
+            response
+            user {
+                id
+            }
+            }
+        }      
+   `;
+  
+    const opts = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ query }),
+    };
+    return fetch(url, opts)
+      .then((res) => res.json())
+      .then((resp) => resp)
+      .catch(console.error);
+};
