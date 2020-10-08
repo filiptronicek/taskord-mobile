cdfba3ccc26a48007340f169bc713fb277c5ffe3
Restyled.io <commits@restyled.io>
2020-10-08 20:01:27 +0000

    Restyled by clang-format

--- src/auth/signin.js
+++ src/auth/signin.js
@@ -1,5 +1,5 @@
 const fetch = require("node-fetch");
-import { api_endpoint as url } from "../consts";
+import {api_endpoint as url} from "../consts";
 
 export const signIn = async (email, password) => {
   const query = `
@@ -15,15 +15,13 @@ export const signIn = async (email, password) => {
    `;
 
   const opts = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
+    method : "POST",
+    headers : {
+      "Content-Type" : "application/json",
     },
-    body: JSON.stringify({ query }),
+    body : JSON.stringify({query}),
   };
 
-  return fetch(url, opts)
-    .then((res) => res.json())
-    .then((resp) => resp);
+  return fetch(url, opts).then((res) => res.json()).then((resp) => resp);
   //.catch(console.error);
 };
