457b759ed66f8c1ae3c499eb225ed1c166a44780
Filip Troníček <filip.tronicek@seznam.cz>
2020-10-25 13:20:01 +0100

    Add placeholders

--- src/app/inputPlaceholder.js
+++ src/app/inputPlaceholder.js
@@ -1,3 +1,3 @@
-const placeholders = ["Buy a new phone", "Buy flowers for my mum", "Brew some coffee", "Ship my awesome product", "Learn about Next.js", "Make a blog post"];
+const placeholders = ["Buy a new phone", "Buy flowers for my mum", "Brew some coffee", "Ship my awesome product", "Learn about Next.js", "Make a blog post", "Buy a USB-c to USB-a cable"];
 
 export const placeHolder = placeholders[Math.floor(Math.random() * placeholders.length)];
